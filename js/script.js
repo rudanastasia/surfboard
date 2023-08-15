804186411ef26e7abba2132071fdd47807c98d10
rudanastasia <135504050+rudanastasia@users.noreply.github.com>
2023-08-15 13:47:50 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ const findBlockByAlias = alias => {
 };
 
 $(".reviews-switcher__link").click(e => {
-    e.preventDefault ();
+    e.preventDefault();
 
     const $this = $(e.currentTarget);
     const target = $this.attr("data-open");
@@ -30,3 +30,45 @@ $(".reviews-switcher__link").click(e => {
     itemToShow.addClass("active").siblings().removeClass("active");
     curItem.addClass("active").siblings().removeClass("active");
 });
+
+
+////
+const openItem = item => {
+    const container = item.closest(".team__item");
+    const contentBlock = container.find(".team__description");
+    const textBlock = contentBlock.find(".team__content");
+    const reqHeight = textBlock.height();
+
+    container.addClass("active");
+    contentBlock.height(reqHeight);
+}
+
+const closeEveryItem = container => {
+    const items = container.find('.team__description');
+    const itemContainer = container.find(".team__item");
+
+    itemContainer.removeClass("active");
+    items.height(0);
+}
+
+$('.team__title').click(e => {
+    const $this = $(e.currentTarget);
+    const container = $this.closest('.team__list');
+    const elemContainer = $this.closest(".team__item");
+
+    if (elemContainer.hasClass("active")) {
+        closeEveryItem(container);
+    } else {
+        closeEveryItem(container);
+        openItem($this);
+    }
+});
+
+/////
+$(".slider__all").bxSlider({
+    pager: false
+});
+
+/*const slider = document.querySelector(".slider__all");
+const buttons = document.querySelectorAll(".slider__btn");
+const items = document.querySelectorAll(".slider__item");*/
