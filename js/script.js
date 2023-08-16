3c68c61307391fb869b92a163a1ed12fdea3b94d
rudanastasia <135504050+rudanastasia@users.noreply.github.com>
2023-08-16 19:01:55 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -64,7 +64,7 @@ $('.team__title').click(e => {
     }
 });
 
-/////
+///// slider
 $(".slider__all").bxSlider({
     pager: false
 });
@@ -72,3 +72,71 @@ $(".slider__all").bxSlider({
 /*const slider = document.querySelector(".slider__all");
 const buttons = document.querySelectorAll(".slider__btn");
 const items = document.querySelectorAll(".slider__item");*/
+
+////// form
+const validateFields = (form, fieldsArray) => {
+    fieldsArray.forEach((field) => {
+        field.removeClass("input-error");
+        if (field.val().trim() == "") {
+            field.addClass("input-error");
+        }
+    });
+
+    const errorFields = form.find(".input-error");
+
+    return errorFields.length == 0;
+}
+
+$('.form').submit(e => {
+    e.preventDefault();
+
+    const form = $(e.currentTarget);
+    const name = form.find("[name='name']");
+    const phone = form.find("[name='phone']");
+    const comment = form.find("[name='comment']");
+    const to = form.find("[name='to']");
+
+    const modal = $("#modal");
+    const content = modal.find(".modal__content");
+
+    modal.removeClass("error-modal");
+
+    const isValid = validateFields(form, [name, phone, comment, to]);
+
+    if (isValid) {
+        const request = $.ajax({
+            url: "",
+            method: "post",
+            data: {
+                name: name.val(),
+                phone: phone.val(),
+                comment: comment.val(),
+                to: to.val(),
+            },
+            error: (data) => {}
+        });
+
+        request.done((data) => {
+            content.text(data.message);
+        });
+
+        request.fail((data) => {
+            const message = data.responseJSON.message;
+                content.text(message);
+                modal.addClass("error-modal");
+        });
+
+        request.always(() => {
+            $.fancybox.open({
+                src: "#modal",
+                type: "inline",
+            })
+        });
+    }
+});
+
+$(".app-submit-btn").click(e => {
+    e.preventDefault();
+
+    $.fancybox.close();
+});
