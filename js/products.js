0b0063feda17f94370039484eac6591b87a1acf8
rudanastasia <135504050+rudanastasia@users.noreply.github.com>
2023-08-20 18:09:49 +0300

    Add files via upload

---
+++ js/products.js
@@ -0,0 +1,19 @@
+const mesureWidth = () => {
+    return 500;
+}
+
+const openItem = item => {
+    const hiddenContent = item.find(".products-menu__content");
+    const reqWidth = mesureWidth();
+
+    hiddenContent.width(reqWidth);
+}
+
+$(".products-menu__title").on("click", e => {
+    e.preventDefault();
+
+    const $this = $(e.currentTarget);
+    const this = $this.closest(".products-menu__item");
+
+    openItem(item);
+});
