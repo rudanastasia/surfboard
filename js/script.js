0154155f1e32d4634f52c90beb82c647887ff375
rudanastasia <135504050+rudanastasia@users.noreply.github.com>
2023-08-25 20:14:48 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -99,7 +99,7 @@ form.addEventListener('submit', (e) => {
 
 
 ///modal
-const openButton = document.querySelector('.hamburger');
+/*const openButton = document.querySelector('.hamburger');
 const modal = document.querySelector('.modal');
 const closeButton = document.querySelector('.modal__close');
 const body = document.querySelector('body');
@@ -112,7 +112,7 @@ openButton.addEventListener('click', () => {
 closeButton.addEventListener('click', () => {
     modal.classList.remove('modal')
     body.classList.remove('body_closed')
-})
+})*/
 
 
 ///////////////////////////////modal window
@@ -275,3 +275,131 @@ const init = () => {
 };
  
 ymaps.ready(init);
+
+
+////mobile-menu
+const burger = $('.hamburger');
+const closeButton = $('.modal__close');
+
+burger.on('click', (e) => {
+    e.preventDefault()
+    $('.modal').show()
+ });
+
+ closeButton.on('click', (e) => {
+    e.preventDefault()
+    $('.modal').hide()
+ });
+
+///////video
+let player;
+const playerContainer = $(".player");
+ 
+let eventsInit = () => {
+ $(".player__start").click(e => {
+   e.preventDefault();
+ 
+   if (playerContainer.hasClass("paused")) {
+     player.pauseVideo();
+   } else {
+     player.playVideo();
+   }
+ });
+ 
+ $(".player__playback").click(e => {
+   const bar = $(e.currentTarget);
+   const clickedPosition = e.originalEvent.layerX;
+   const newButtonPositionPercent = (clickedPosition / bar.width()) * 100;
+   const newPlaybackPositionSec =
+     (player.getDuration() / 100) * newButtonPositionPercent;
+ 
+   $(".player__playback-button").css({
+     left: `${newButtonPositionPercent}%`
+   });
+ 
+   player.seekTo(newPlaybackPositionSec);
+ });
+ 
+ $(".player__splash").click(e => {
+   player.playVideo();
+ })
+};
+ 
+const formatTime = timeSec => {
+ const roundTime = Math.round(timeSec);
+ 
+ const minutes = addZero(Math.floor(roundTime / 60));
+ const seconds = addZero(roundTime - minutes * 60);
+ 
+ function addZero(num) {
+   return num < 10 ? `0${num}` : num;
+ }
+ 
+ return `${minutes} : ${seconds}`;
+};
+ 
+const onPlayerReady = () => {
+ let interval;
+ const durationSec = player.getDuration();
+ 
+ $(".player__duration-estimate").text(formatTime(durationSec));
+ 
+ if (typeof interval !== "undefined") {
+   clearInterval(interval);
+ }
+ 
+ interval = setInterval(() => {
+   const completedSec = player.getCurrentTime();
+   const completedPercent = (completedSec / durationSec) * 100;
+ 
+   $(".player__playback-button").css({
+     left: `${completedPercent}%`
+   });
+ 
+   $(".player__duration-completed").text(formatTime(completedSec));
+ }, 1000);
+};
+ 
+const onPlayerStateChange = event => {
+ /*
+   -1 (воспроизведение видео не начато)
+   0 (воспроизведение видео завершено)
+   1 (воспроизведение)
+   2 (пауза)
+   3 (буферизация)
+   5 (видео подают реплики).
+ */
+ switch (event.data) {
+   case 1:
+     playerContainer.addClass("active");
+     playerContainer.addClass("paused");
+     break;
+ 
+   case 2:
+     playerContainer.removeClass("active");
+     playerContainer.removeClass("paused");
+     break;
+ }
+};
+ 
+function onYouTubeIframeAPIReady() {
+ player = new YT.Player("yt-player", {
+   height: "400px",
+   width: "100%",
+   videoId: "LXb3EKWsInQ",
+   events: {
+     onReady: onPlayerReady,
+     onStateChange: onPlayerStateChange
+   },
+   playerVars: {
+     controls: 0,
+     disablekb: 0,
+     showinfo: 0,
+     rel: 0,
+     autoplay: 0,
+     modestbranding: 0
+   }
+ });
+}
+ 
+eventsInit();
